0f2a1671a3ae7c93185c7d2a9426996921510ea5
FVladislove <f.vlad.is.love@gmail.com>
2022-09-09 12:03:40 +0300

    added MVC elements

---
+++ MusicManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MusicManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
